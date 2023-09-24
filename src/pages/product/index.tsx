a9d7ba97bce196930a03df8dca42cfc8c34ffc00
dodisutarya <ikhwands@protonmail.ch>
2023-09-24 19:56:33 +0700

    API Routes, storage firebase

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -2,8 +2,16 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 const ProductPage = () => {
-  const [isLogin, setIsLogin] = useState();
+  const [isLogin, setIsLogin] = useState(true);
+  const [products, setProducts] = useState([]);
   const { push } = useRouter();
+
+  type productType = {
+    id: number;
+    name: string;
+    price: number;
+    size: string;
+  };
 
   useEffect(() => {
     if (!isLogin) {
@@ -11,7 +19,22 @@ const ProductPage = () => {
     }
   }, []);
 
-  return <div>ProductPage</div>;
+  useEffect(() => {
+    fetch("/api/product").then((res) => {
+      res.json().then((response) => {
+        setProducts(response.data);
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Product Page</h1>
+      {products.map((product: productType) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductPage;
--- src/views/Auth/Login/index.tsx
+++ src/views/Auth/Login/index.tsx
@@ -4,15 +4,23 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 const LoginViews = () => {
-  const { push } = useRouter();
+  // const { push } = useRouter();
+  const router = useRouter();
   const handleLogin = () => {
-    push("/product");
+    // push("/product")
+    console.log(router);
+    router.push("/product");
   };
 
   return (
     <div className={styles.login}>
       <h1 className="text-3xl font-bold">Login Page</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleLogin()}>Login</button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => handleLogin()}
+      >
+        Login
+      </button>
       <p style={{ color: "red", border: "1px solid red", borderRadius: "5px" }}>
         Belum punya akun? registrasi <Link href={"/auth/register"}>disini</Link>
       </p>
